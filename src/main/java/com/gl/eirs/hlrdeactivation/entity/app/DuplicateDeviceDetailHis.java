c734f0c67d595750dfcfa453e631c5843a4b3160
KunalMathuriya <kunalmathuria27@gmail.com>
2024-06-25 12:28:33 +0530

    updated table name for duplicate_device_detail_his table

--- src/main/java/com/gl/eirs/hlrdeactivation/entity/app/DuplicateDeviceDetailHis.java
+++ src/main/java/com/gl/eirs/hlrdeactivation/entity/app/DuplicateDeviceDetailHis.java
@@ -8,7 +8,7 @@ import java.time.LocalDateTime;
 
 @Entity
 @Data
-@Table(name= "duplicate_device_detail_His")
+@Table(name= "duplicate_device_detail_his")
 public class DuplicateDeviceDetailHis {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
